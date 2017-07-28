7954ed7a9084f9e81fa7560766d423c3430df10c
Anupreksha jain <anuprekshajain9@gmail.com>
2017-07-28 18:19:27 +0530

    Bug fixes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ var upload = document.getElementsByTagName('input')[0],
 
 if (typeof window.FileReader === 'undefined') {
   state.className = 'fail';
-} 
+} else {
+  state.className = 'success';
+  state.innerHTML = 'File API & FileReader available';
+}
 
 upload.onchange = function (e) {
   e.preventDefault();
